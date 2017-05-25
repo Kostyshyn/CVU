415808f0f30038d6f9015b7719d846f3eb95e6f1
vovadakhniy <vova.dakhniy@gmail.com>
2017-05-26 00:46:02 +0300

    styling main content+js slider

---
+++ app/js/slider.js
@@ -0,0 +1,39 @@
+// var i=0;
+// function showPrevious(){
+// 	i=i-1; 
+// 	if (i<0) {
+// 		i=sliders.length-1;
+// 	}
+// 	var image=document.getElementById('slide-image'); 
+// 	var title=document.getElementById('slide-title'); 
+// 	var description=document.getElementById('slide-description');
+// 	image.src=sliders[i].image;
+// 	title.innerHTML=sliders[i].title;
+// 	description.innerHTML=sliders[i].description;
+// }
+
+// function showNext(){
+// 	i=i+1; 
+// 	if (i>=sliders.length) {
+// 		i=0;
+// 	}
+// 	var image=document.getElementById('slide-image'); 
+// 	var title=document.getElementById('slide-title'); 
+// 	var description=document.getElementById('slide-description');
+// 	image.src=sliders[i].image;
+// 	title.innerHTML=sliders[i].title;
+// 	description.innerHTML=sliders[i].description;
+// }
+
+// var sliders = [
+// {
+// 	title:"За час роботи ВР VIII скликання Президент заветував 39 законів, ..",
+// 	description:"Із початку роботи Верховної Ради VIII скликання Президент наклав вето на 39 законів. Ініціаторами більшості із них, 31 закону, були народні депутати, ще 8 законів ініціював уряд (А. Яценюка). Про це йдеться в дослідженні КВУ «Три роки президентства П. Порошенка...",
+// 	image:"http://cvu.org.ua/uploads/59257686-9830-4d42-a69b-6b400a0a0d19-%D1%96%D0%BD%D1%843.png"
+// },
+// {
+// 	title:"Які фракції підтримують законопроекти Президента, - дослідження КВУ",
+// 	description:"Народний Фронт, Блок Петра Порошенка та «Самопоміч» найбільше підтримують законопроекти Президента. Про це зазначив голова КВУ Олексій Кошель, презентуючи дослідження КВУ: «Три роки президентства П. Порошенка: які закони блокує та які підтримує През...",
+// 	image:"http://cvu.org.ua/uploads/592566cd-72fc-43c4-9fb2-69a60a0a0d19-%D1%96%D0%BD%D1%842.png"
+// }
+// ]; 
