e8a444d14cb0102959997ad8a418eae080b7d569
Andriy007 <andriy087@gmail.com>
2017-06-02 11:42:57 +0300

    Json+fix

---
+++ app/js/project.js
@@ -0,0 +1,39 @@
+var mediaWrap = document.getElementById('project-blocks');
+if (mediaWrap) {
+    var ROOT = 'data/project.json';
+
+    var mediaTemplate = new Template([
+        '<div>',
+        '<a class="projecTitle" href="{{src}}">',
+        '<h3>{{ title }}</h3>',
+        '</a>',
+        '<div class="projectInfo">',
+        '<img width="100%" height="auto" src="{{image}}" alt="">',
+        '</div>',
+        '<div class="projectContent">',
+        '<p>{{text}}</p>',
+        '<div class="tags">',
+        '<div class="marks">',
+        '<span>Мітка:</span>',
+        '<a href="" class="mark-analitic">{{ label }}</a>',
+        '</div>',
+        '</div>'
+    ]);
+
+    getData('GET', ROOT, function (err, data) {
+        if (err) {
+            alert(err.message);
+            return;
+        } else {
+            mediaTemplate.render(data, function (err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    mediaWrap.innerHTML = result;
+                }
+            });
+        }
+    });
+}
+
+
